1eacb208552d7156bfa68f811d14ee42e42f517a
Nathan Ford <nathan@markboultondesing.com>
2012-03-13 09:45:10 +0000

    2.0 with an all new load test

--- js/pngy.js
+++ js/pngy.js
@@ -1,101 +1,94 @@
-// Pngy by Nathan Ford - pngy.artequalswork.com
+// pngy by Nathan Ford - pngy.artequalswork.com
 
 pngy = function (ob) {
 	
 	var defaults = {
-		fast: 180,
-		normal: 300,
-		slow: 'all'
-	},	
-	p = {
-		file : (ob && ob.file != undefined) ? ob.file : '1k.gif',
-		test_repeat : (ob && ob.test_repeat != undefined) ? ob.test_repeat : 3,
-		selector : (ob && ob.selector != undefined) ? ob.selector : '.pngy',
-		display_results: (ob && ob.display_results != undefined) ? ob.display_results : false,
-		limits : (ob && ob.limits != undefined) ? ob.limits : defaults,
-		i : 0,
-		nm : 0
-	};
+		loadspeed : false,
+		loadsize : false,
+		selector : '.pngy',
+		display_results : false,
+		apply_to_all_imgs: false,
+		limits : {
+			fast : 40,
+			normal : 10,
+			slow : 0
+		},
+		base: 'slow'
+	}, p = new Object(), inittime;
 	
-	if (ob.apply_to_all_imgs) p.selector += ', img';
+	// pull in custom setting or use defaults
+	if (ob) for (k in defaults) p[k] = (ob[k] != undefined) ? ob[k] : defaults[k];
+	else p = defaults;
 	
+	// add all IMGs to p.selector if apply_to_all_imgs is true
+	if (p.apply_to_all_imgs) p.selector += ', img';
+	
+	// get all elements that match the selector
 	p.imgs = $(p.selector);
 	
-	results( p );
-	
-};
-
-results = function ( p ) {
-	
-	if (p.file) {
-	
-		var sd = new Date();
-		var sm = sd.getTime();
+	if ($(p.imgs).length) { // only run the rest if there are elements that match p.selector
 		
-		$("<img />").attr('src', p.file + '?' + sm).load(function () {
+		var pngy1 = (p.imgs[0].tagName == 'IMG') ? $(p.imgs[0]).attr('src') : $(p.imgs[0]).css('background-image').replace('url("','').replace('")','');
+		
+		$.ajax({
+			url: pngy1 + '?' + Math.random(), // pngy1
+			beforeSend: function () {
+				
+				// get time before ajax request
+				var initdate = new Date();
+				inittime = initdate.getTime();
+				
+			},
+			success: function (d) {
 			
-			if (this.complete && typeof this.naturalWidth != "undefined" && this.naturalWidth != 0){
+				var loaddate = new Date(), limits = [], i = 0, j = 0;
 				
-				var time = new Date().getTime() - sm;
-				p.nm += time;
+				// get image size in Kb
+				if (!p.loadsize) p.loadsize = d.length / 1024;
 				
-				p.i++;
+				// get loadspeed in seconds
+				if (!p.loadspeed) p.loadspeed = (loaddate.getTime() - inittime) / 1000; 
 				
-				if (p.i < p.test_repeat) results( p );
-				else {
+				// get Kbs
+				p.mbs = Math.floor((p.loadsize / p.loadspeed) * 100) / 100; 
 				
-					if (p.nm != 0) { 
-						
-						p.avg = Math.round(p.nm / p.test_repeat); 
-						
-						for (k in p.limits) {
+				// output speed if display_results has a selector
+				$(p.display_results).html(p.mbs + ' Kbs'); 
+				
+				// uncomment to get a log of your load speed
+				//console.log('Load speed: ' + p.mbs + ' Kbs'); 
+				
+				// reorder limits to highest to lowest
+				for (k in p.limits) limits.push([k, p.limits[k]]);
+				limits.sort(function(a, b) {return a[1] - b[1]});
+				limits.reverse();
+				
+				// find the limit
+				while (limits[i]) {
+					p.speed = limits[i][0];
+					if (limits[i][1] < p.mbs) break;
+					i++;
+				}
+				
+				p.base = limits[limits.length-1][0];
+				
+				// edit image paths
+				while( p.imgs[j] ) {
+					
+					var t = p.imgs[j], reg = new RegExp("(\-" + p.base + "\.([A-Za-z]{3,4})\"*)$");
+					
+					if (t.tagName == 'IMG')
+						$(t).attr('src', $(t).attr('src').replace(reg, '-' + p.speed + ".$2"));
+					else
+						$(t).css('background-image', 'url(' + $(t).css('background-image').replace('url("','').replace('")','').replace(reg, '-' + p.speed + ".$2") + ')');
 							
-							if (!k.match('_suff')) {
-							
-								var sp = p.limits[k];
-								
-								if (sp > p.avg) {
-									p.speed = k;
-									break;
-								}
-								else p.speed = k;
-							
-							} 
-						
-						}
-						
-						// change suffix for speed
-						if (p.limits[p.speed] !== 'all') appendimgs( p );
-						
-						// place speed results in selector
-						$(p.display_results).html(p.speed + ' (' + p.avg + ')');
-											
-					}
+					j++;
 					
 				}
-			
-			}
-			
-		});
-		
-	}
-	
-};
-
-appendimgs = function (p) {
-	
-	var s = p.speed, j = 0, reg = new RegExp("((\-[^\-]+)\.([A-Za-z]{3,4})\"*)$");
-	
-	while( p.imgs[j] ) {
-		
-		var t = p.imgs[j];
-		
-		if (t.tagName == 'IMG')
-			$(t).attr('src', $(t).attr('src').replace(reg, '-' + s + ".$3"));
-		else
-			$(t).css('background-image', 'url(' + $(t).css('background-image').replace('url("','').replace('")','').replace(reg, '-' + s + ".$3") + ')');
 				
-		j++;
+			}
+		
+		});
 		
 	}
 
